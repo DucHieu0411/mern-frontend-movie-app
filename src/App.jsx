b90329f33da63d890fe0433c43504ab192422a5f
DucHieu0411 <107285882+DucHieu0411@users.noreply.github.com>
2023-07-26 16:43:11 +0700

    create App Layout and Routing with React Router V6

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import themeConfigs from "./configs/theme.configs";
 import { ToastContainer } from "react-toastify";
 import CssBaseline from "@mui/material/CssBaseline";
 
+import routes from "./routes/routes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import PageWrapper from "./components/common/PageWrapper";
+
 const App = () => {
   const { themeMode } = useSelector((state) => state.themeMode);
 
@@ -22,6 +27,46 @@ const App = () => {
       />
       {/* MUI reset CSS */}
       <CssBaseline />
+
+      {/* App routes */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            {routes.map((route, index) =>
+              route.index ? (
+                <Route
+                  index
+                  key={index}
+                  element={
+                    route.state ? (
+                      <PageWrapper state={route.state}>
+                        {route.element}
+                      </PageWrapper>
+                    ) : (
+                      route.element
+                    )
+                  }
+                />
+              ) : (
+                <Route
+                  path={route.path}
+                  key={index}
+                  element={
+                    route.state ? (
+                      <PageWrapper state={route.state}>
+                        {route.element}
+                      </PageWrapper>
+                    ) : (
+                      route.element
+                    )
+                  }
+                />
+              )
+            )}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* App routes */}
     </ThemeProvider>
   );
 };
