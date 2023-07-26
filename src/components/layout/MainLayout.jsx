3e456475653ae2ed79a39c4746913decb022e6d9
DucHieu0411 <107285882+DucHieu0411@users.noreply.github.com>
2023-07-26 16:59:38 +0700

    create Global Loading

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import GlobalLoading from "../common/GlobalLoading";
 
 const MainLayout = () => {
   return (
     <>
       {/* Global loading */}
+      <GlobalLoading />
       {/* Global loading */}
 
       {/* Login modal */}
